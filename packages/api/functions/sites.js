b5230364b6d9a7826eb58d36c99a00c0393d338a
hughes-ch <contact@chrishughesdev.com>
2021-11-15 18:00:09 -0500

    API hello world

---
+++ packages/api/functions/sites.js
@@ -0,0 +1,12 @@
+/**
+ *   Defines the function for GET/PUT/POST /sites
+ *
+ *   :copyright: Copyright (c) 2021 Chris Hughes
+ *   :license: MIT License
+ */
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: 'Hello world!',
+  };
+};
